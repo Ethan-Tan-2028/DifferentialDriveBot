100a8fceba75af043b1f88ff20bf24bf36a7e6f4
Ethan-Tan-2028 <ethant153@nycstudents.net>
2024-12-09 16:21:02 -0500

    Update and rename Drive.java to Ports.java

---
+++ Drive/Ports.java
@@ -0,0 +1,21 @@
+package robot;
+package robot.drive;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class Drive extends SubsystemBase {
+
+}
+public final class Ports {
+  // TODO: Add and change all ports as needed.
+  public static final class OI {
+    public static final int OPERATOR = 0;
+    public static final int DRIVER = 1;
+  }
+
+
+}
