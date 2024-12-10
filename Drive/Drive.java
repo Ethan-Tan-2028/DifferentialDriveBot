8660491a6c13a06e10056802a909dd611c13d789
Ethan-Tan-2028 <ethant153@nycstudents.net>
2024-12-09 23:49:27 -0500

    Update Drive.java

--- Drive/Drive.java
+++ Drive/Drive.java
@@ -1,1 +1,22 @@
+import.List
+public class Drive extends SubsystemBase {  
+  private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
+  private final CANSparkMax leftfollower = new CANSparkMax(Ports.Drive.LEFT_FOLLOWER, MotorType.kBrushless);
+  private final CANSparkMax rightLeader = new CANSparkMax(Ports.Drive.RIGHT_LEADER, MotorType.kBrushless);
+  private final CANSparkMax rightfollower = new CANSparkMax(Ports.Drive.RIGHT_FOLLOWER, MotorType.kBrushless);
+
+    public Drive() {
+    for (CANSparkMax spark : List.of(leftLeader, leftFollower, rightLeader, rightFollower)) {
+	    spark.restoreFactoryDefaults();
+      spark.setIdleMode(IdleMode.kBrake);
+
+    }
+    rightFollower.follow(rightLeader);
+    leftFollower.follow(leftLeader);
+    leftLeader.setInverted(true);
+	  rightLeader.setInverted(true);
+
+  }
+
+}
 
