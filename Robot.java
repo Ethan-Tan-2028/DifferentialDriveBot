99c4dfbe1d776996710ad1406bb5573060c71655
Ethan-Tan-2028 <ethant153@nycstudents.net>
2024-12-09 23:55:51 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,3 +1,5 @@
 public Class Robot{
-  public Command drive(DoubleSupplier vLeft, DoubleSupplier vRight);
+  public Command drive(DoubleSupplier vLeft, DoubleSupplier vRight) {
+    return run(() -> drive(vLeft.getAsDouble(), vRight.getAsDouble()));
+  }
 }
