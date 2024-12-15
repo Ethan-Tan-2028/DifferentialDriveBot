936b91e5f88dcab68484d3907a9340a1d1ecdb83
Ethan-Tan-2028 <ethant153@nycstudents.net>
2024-12-15 18:39:11 -0500

    Delete Robot directory

--- Robot/Robot.java
+++
@@ -1,8 +0,0 @@
-public Class Robot{
-  public Command drive(DoubleSupplier vLeft, DoubleSupplier vRight) {
-  }
-    Drive drive = new Drive();
-  private void configureBindings() {
-	drive.setDefaultCommand(drive.drive(driver::getLeftY, driver::getRightY));
-}
-}
