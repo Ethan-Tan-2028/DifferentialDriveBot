ee8199c68203e5a013d34c656fff43c1446a325a
Ethan-Tan-2028 <ethant153@nycstudents.net>
2024-12-15 18:33:08 -0500

    Update Ports.java

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -1,9 +1,26 @@
 package robot;
+package robot.drive;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class Drive extends SubsystemBase {
+
+}
 public final class Ports {
   // TODO: Add and change all ports as needed.
   public static final class OI {
     public static final int OPERATOR = 0;
     public static final int DRIVER = 1;
   }
+  public static final class Drive {
+    public static final int LEFT_LEADER = 1;
+    public static final int LEFT_FOLLOWER = 2;
+    public static final int RIGHT_LEADER = 3;
+    public static final int RIGHT_FOLLOWER = 4;
+  }
+
 }
